14418f13338cb3986a5a289c3d0209877f96c201
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-07-22 17:55:18 -0700

    Fading

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,30 @@
 	$(document).ready(function(){
+		
+		$(".site-wrapper").fadeIn({
+			"duration": 200,
+			//"easing": "easeInBounce",
+
+		});
 		$(".defaultLink").click(function(event){
 			event.preventDefault();
-			//alert("prevented default");
-			console.log($(".active a").css("color"));
-			$the_white_color = "rgb(255, 255, 255)"
-			if($(".active a").css("color") == $the_white_color){
-				$(".active a").css("color", "red");
-			} else {
-				$(".active a").css("color", $the_white_color);
-			}
-			
-		})
+			//$("p.lead").fadeOut(6000);
+			//$("li:nth-child(2) a").fadeOut(400);
+		});
+		$("#features").click(function(event){
+			event.preventDefault();
+			$(".main-content").fadeToggle("fast", function(){
+				$(this).html("<h1>Features</h1>");
+			}).fadeToggle("fast");
+			//$("li:nth-child(2) a").fadeOut(400);
+		});
+
+		$("#contact").click(function(event){
+			event.preventDefault();
+			$(".main-content").fadeToggle("fast", function(){
+				$(this).html("<h1>Contact Us</h1>");
+			}).fadeToggle("fast");
+			//$("li:nth-child(2) a").fadeOut(400);
+		});
+
 
 	});
